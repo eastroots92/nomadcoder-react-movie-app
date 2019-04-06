b63ee30b32854d79f242a9670398cc014caf8df7
eastroots92 <eastroots92@gmail.com>
2019-04-06 09:00:35 +0900

    Add movie props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Movie from './Movie';
 
+const movies = [1, 2, 3, 4, 5];
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie/>
+        {movies.map((movie, index) => (
+            <Movie key={index}
+                   title='hello'
+                   poster='https://amp.businessinsider.com/images/56e0323fdd089575198b45bc-750-375.jpg'
+            />
+        ))};
       </div>
     );
   }
